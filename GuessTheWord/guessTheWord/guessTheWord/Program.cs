a9c3c7efb6532498b462bf9ef7b7edf083251604
Dani <danielpinto15@gmail.com>
2020-10-14 22:30:03 +0100

    added simple guessing game

---
+++ GuessTheWord/guessTheWord/guessTheWord/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace guessTheWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string secretWord = "cenas";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter your guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if (outOfGuesses)
+            {
+                Console.Write("You LOSE!");
+            }
+            else
+            {
+                Console.Write("You Win, the scret word was: " + secretWord);
+            }
+            
+
+
+
+
+        }
+    }
+}
