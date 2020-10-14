c34739656ec3c89271b867f294ec266a5ae2b9ad
Dani <danielpinto15@gmail.com>
2020-10-14 21:54:53 +0100

    added simple calculator

---
+++ simpleCalculator/learning csharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace learning_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            switch (op)
+            {
+                case"+":
+                    Console.Write(num1 + num2);
+                    break;
+                case "-":
+                    Console.Write(num1 - num2);
+                    break;
+                case "*":
+                    Console.Write(num1 * num2);
+                    break;
+                case "/":
+                    Console.Write(num1 / num2);
+                    break;
+                default:
+                    Console.Write("Invalid Operator");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
