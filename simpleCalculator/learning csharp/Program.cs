7b5808066ec49d0ccfc00e8bbe684a9e570e04ad
Dani <danielpinto15@gmail.com>
2020-10-15 11:22:09 +0100

    added some exceptions

--- simpleCalculator/learning csharp/Program.cs
+++ simpleCalculator/learning csharp/Program.cs
@@ -6,35 +6,52 @@ namespace learning_csharp
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter an operator: ");
-            string op = Console.ReadLine();
-
-            Console.Write("Enter a number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            switch (op)
+            while (true)
             {
-                case"+":
-                    Console.Write(num1 + num2);
-                    break;
-                case "-":
-                    Console.Write(num1 - num2);
-                    break;
-                case "*":
-                    Console.Write(num1 * num2);
-                    break;
-                case "/":
-                    Console.Write(num1 / num2);
-                    break;
-                default:
-                    Console.Write("Invalid Operator");
-                    break;
-            }
+                try
+                {
+                    Console.Write("Enter a number: ");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.ReadLine();
+                    Console.Write("Enter an operator: ");
+                    string op = Console.ReadLine();
+
+                    Console.Write("Enter a number: ");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+
+                    switch (op)
+                    {
+                        case "+":
+                            Console.Write(num1 + num2);
+                            break;
+                        case "-":
+                            Console.Write(num1 - num2);
+                            break;
+                        case "*":
+                            Console.Write(num1 * num2);
+                            break;
+                        case "/":
+                            Console.Write(num1 / num2);
+                            break;
+                        default:
+                            Console.Write("Invalid Operator");
+                            break;
+                    }
+
+                }
+
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.ReadLine();
+            }
         }
         
     }
